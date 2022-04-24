7e9c0f29c9308a65661612b194e0bfc6644173ca
arjun-ms <arjunachu123.aa@gmail.com>
2022-04-24 17:32:32 +0530

    Verified

--- polynomialaddition.c
+++ polynomialaddition.c
@@ -2,46 +2,64 @@
 
 int main(){
 	
-	float poly1[7],poly2[7];
-	int d1=5,d2=3;
-	float respoly[20];
+	int poly1[50],poly2[50],respoly[50];
+	int d1,d2;
+	int i;
+	int maxdeg,mindeg;
 	
-	for(int i=d1;i>=0;i--){
-		printf("Enter coeff of x^%d: ",i);
-		scanf("%f",&poly1[i]);
+	// POLY1
+	printf("Enter the degree of poly1: ");
+	scanf("%d",&d1);
+	
+	printf("Enter the Coefficients for poly1: ");
+	for(i=d1;i>=0;i--){
+		printf("\nx^%d: ",i);
+		scanf("%d",&poly1[i]);
 	}
 	
-	for(int i=d2;i>=0;i--){
-		printf("Enter coeff of x^%d: ",i);
-		scanf("%f",&poly2[i]);
+	
+	// POLY 2
+	printf("Enter the degree of poly2: ");
+	scanf("%d",&d2);
+	
+	printf("Enter the Coefficients for poly2: ");
+	for(i=d2;i>=0;i--){
+		printf("\nx^%d: ",i);
+		scanf("%d",&poly2[i]);
 	}
 	
+	// printing user-input polynomials
+	printf("\nPoly1 : "  );
+	for(i=d1;i>=0;i--){
+		printf("%dx^%d + ",poly1[i],i);
+	}
+	printf("\nPoly2 : "  );
+	for(i=d2;i>=0;i--){
+		printf("%dx^%d + ",poly2[i],i);
+	}
 	
-	int ld = d1>d2 ? d1:d2;
-	int sd = d1<d2 ? d1:d2;
-	 
-	for(int i=0;i<=sd;i++){
-			respoly[i] = poly1[i]+poly2[i]; 
+	maxdeg = d1>d2 ? d1:d2;
+	mindeg = d1<d2 ? d1:d2;
+	
+	// LOGIC
+	for(i=0;i<=mindeg;i++){
+		respoly[i]=poly1[i]+poly2[i];
 	}
 	
 	if(d1>d2){
-		for(int i=sd+1;i<ld;i++){
-			respoly[i] = poly1[i]; 
+		for(i=mindeg+1;i<=maxdeg;i++){
+			respoly[i]=poly1[i];
+		}
+	}
+	else if(d1<d2){
+		for(i=mindeg+1;i<=maxdeg;i++){
+			respoly[i]=poly2[i];
 		}
 	}
 	
-	else if(d2>d1){
-		for(int i=sd+1;i<ld;i++){
-			respoly[i] = poly2[i]; 
-		}
+	printf("\nSum: ");
+	for(int i=maxdeg;i>=0;i--)
+	{
+		printf(" %dx^%d +",respoly[i],i);
 	}
-	
-	printf("sum: ");
-	for(int i=ld;i>=0;i--){
-			printf("%.2fx^%d + ",respoly[i],i) ;
-	}
-	
-	
-	
-	
 }
