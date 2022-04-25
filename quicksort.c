01deb43a99e7fe284121791cf9a607650f70208e
arjun-ms <arjunachu123.aa@gmail.com>
2022-04-25 08:22:13 +0530

    Verified

--- quicksort.c
+++ quicksort.c
@@ -1,17 +1,68 @@
-#include <stdio.h>  
+#include <stdio.h> 
 
-int partition(int a[],int start,int end){
-	
-	int pivot = a[end];
-	int i = start-1;
-	
-	for(int j = start;j<end-1;j++){
-		if(a[j]<pivot){
-			i++;
-			int temp = a[i];
-			a[i] = a[j]
-			a[j] = temp;
+// function to swap elements
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+int partition(int a[],int low,int high){
+		int pivot = a[low];
+		int start=low;
+		int end=high;
+		
+		while(start<end){
+				
+			while(a[start]<=pivot){
+				start++;
+			};
+				
+			while(a[end]>pivot){
+				end--;
+			};
+			
+			if(start<end){
+				swap(&a[start],&a[end]);
+			}
 		}
-	}
-	int temp = a[i+1]
+		swap(&a[low],&a[end]);
+		return end;
+}
+void quickSort(int a[],int low,int high){
+		if(low<high){
+			
+			// find the pivot element such that
+			// elements smaller than pivot are on left of pivot
+			// elements greater than pivot are on right of pivot
+			int pi = partition(a,low,high);
+			// left section of pivot
+			quickSort(a,low,pi-1);
+			// right section of pivot
+			quickSort(a,pi+1,high);
+
+		}
+}
+
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+
+
+int main(){
+		int a[] = {8, 7, 2, 1, 0, 9, 6};
+		int n = sizeof(a)/sizeof(a[0]);
+		printf("%d",n);
+		
+		printf("\nUnsorted Array: \n");
+		printArray(a,n);
+		
+		quickSort(a,0,n-1);
+		
+		printf("\nSorted Array: \n");
+		printArray(a,n);
+		
 }
